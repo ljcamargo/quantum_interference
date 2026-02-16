6181cf55833e091c2ad7385a417ebec8427c41d4
Luis J Camargo <lsjcp@yahoo.com>
2026-02-16 14:24:11 -0600

    type and css fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Doto:wght@100..900&family=SUSE+Mono:ital,wght@0,100..800;1,100..800&display=swap" rel="stylesheet" />
+      </head>
       <body className="antialiased">
         {children}
       </body>
